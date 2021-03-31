b7f33bb553afb3c28aa087a57d0c1209a1afadb9
Sai Sunku <sai2020@gmail.com>
2021-03-31 13:22:22 -0400

    Make install_ycm.sh run in the correct directory
    install_ycm.sh must run in the directory of the script,
    not the home directory for git submodule update to run
    correctly.

--- install_ycm.sh
+++ install_ycm.sh
@@ -1,6 +1,8 @@
 # Compile the YouCompleteMe code with Python
 
 # First clone all the submodules
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Directory of the installation script
+cd $SCRIPT_DIR
 git submodule update --init --recursive
 
 sudo apt-get install python3-dev cmake g++
